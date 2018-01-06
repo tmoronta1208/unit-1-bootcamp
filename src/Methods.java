6c6601eadb25cac0075b7e87aa0ff10adb2baa81
Tatiana Moronta <tatianamoronta@ac.c4q.nyc>
2018-01-06 14:22:18 -0500

    methods

--- src/Methods.java
+++ src/Methods.java
@@ -1,9 +1,50 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by alexandraqin on 4/14/15.
  */
 public class Methods {
 
-  public static void main (String args[]) {
+    public static void main(String args[]) {
+        prettyInteger(10);
+    }
 
-  }
+
+    public static int calculateSquare(int a) {
+
+        return a * a;
+    }
+
+
+    public static double calculateSquareRoot(double b) {
+
+
+        return Math.sqrt(b);
+    }
+
+    public static String toLowerCase(String c) {
+
+        return c.toLowerCase();
+    }
+
+    public static boolean isMultiple(int a, int b) {
+
+        return b % a == 0;
+    }
+
+    public static void prettyInteger(int a) {
+
+        String star = "*";
+        int b = a * 2;
+        List<String> starArray = new ArrayList<>();
+        for (int i = 0; i < b; i++) {
+            starArray.add(star);
+
+            if (starArray.size() == a) {
+                starArray.add(a,a+"");
+            }
+        }
+        System.out.println(starArray);
+    }
 }
--- src/Variables.java
+++ src/Variables.java
@@ -1,10 +1,39 @@
+import com.sun.xml.internal.bind.v2.TODO;
+
 /**
  * Created by alexandraqin on 4/14/15.
  */
 public class Variables {
 
-  public static void main (String args[]) {
+    public static void main(String args[]) {
 
 
-  }
+    }
+
+
+    public static void usingVariables(int a, double b, String c) {
+
+        System.out.println("This is room # " + a);
+        System.out.println("e is close to " + b);
+        System.out.println("I am learning a bit about " + c);
+    }
+
+    public static boolean isOdd(int b) {
+
+        return b % 2 == 1;
+
+    }
+
+    public static void printHelloWorld() {
+        System.out.println("Hello World");
+    }
+
+    public static void greatestCommonFactor(int a, int b) {
+
+        //TODO
+        //Complete method
+
+    }
 }
+
+
